040431a66d844e4a2903e8e888ae58af3c14c5a7
DevIcaro16 <icaroip15@gmail.com>
2025-05-20 01:35:31 -0300

    Modify Package.json - Commit 16

--- dist/server.js
+++ dist/server.js
@@ -25,6 +25,7 @@ app.use(function (err, req, res, next) {
         status: 'error',
         message: 'Internal server error.'
 
+
     });
 });
 var port = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3333;
